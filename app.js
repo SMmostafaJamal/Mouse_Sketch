c30c2031e4dd63ffec16c7a6adfac49c3c9e9c86
sm.mostafajamal <sm.mostafajamal@gmail.com>
2022-01-01 15:17:55 +0600

    added clear button and resize square

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 const container = document.getElementById('container');
 const box = document.createElement('div');
 const color = document.getElementById('color');
+const clear = document.getElementById('clean')
 container.insertAdjacentElement('beforeend', box)
 box.setAttribute('id', 'box')
 // box.classList.add('square');
 
-
+const numberOfSquares = parseInt(prompt('number of squares?'))
 // creating 16 main divs/squares
-
-    for(let i = 0; i < 16; i++){
-        const parentGrid = document.createElement('div');
+function createSquares(number){
+    for(let i = 0; i < number; i++){
+        const parentGrid = document.createElement('div')
         box.insertAdjacentElement('beforeend', parentGrid)
         parentGrid.classList.add('sibling');
         // inside main divs/squares creating 16 divs in total of 16 X 16 divs
-        for(let j = 1; j <= 16; j++){
+        for(let j = 1; j <= number; j++){
             const childGrid = document.createElement('div');
             parentGrid.insertAdjacentElement('beforeend', childGrid);
             childGrid.classList.add('square');
-            childGrid.addEventListener('click', function(e){
-                if(e.type == 'click'){
-                    this.type = 'mouseover'
-                    console.log(e.type)
-                // this.addEventListener('mouseover',function(e){
-                //     this.style.backgroundColor = color.value
-                // })    
-                }
+            childGrid.addEventListener('mouseover', function(e){
+                this.style.backgroundColor = color.value
+            })
+            clear.addEventListener('click', function(e){
+                childGrid.style.backgroundColor = 'white'
+                // console.log(this.classList)
             })
         }
     }
-
+}
+createSquares(numberOfSquares)
