3d424f9b503a26720d17d9655dc54b4b975febf9
sm.mostafajamal <sm.mostafajamal@gmail.com>
2022-01-06 22:36:54 +0600

    changed code

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
-const container = document.getElementById('container');
-const box = document.createElement('div');
-const color = document.getElementById('color');
-const clear = document.getElementById('clean')
-container.insertAdjacentElement('beforeend', box)
-box.setAttribute('id', 'box')
-// box.classList.add('square');
+const grid = document.querySelector('#grid');
+const color = document.querySelector('#color');
+const clean = document.querySelector('#clean');
+const range = document.querySelector('#rangeValue')
 
-const numberOfSquares = parseInt(prompt('number of squares?'))
-// creating 16 main divs/squares
-function createSquares(number){
-    for(let i = 0; i < number; i++){
-        const parentGrid = document.createElement('div')
-        box.insertAdjacentElement('beforeend', parentGrid)
-        parentGrid.classList.add('sibling');
-        // inside main divs/squares creating 16 divs in total of 16 X 16 divs
-        for(let j = 1; j <= number; j++){
-            const childGrid = document.createElement('div');
-            parentGrid.insertAdjacentElement('beforeend', childGrid);
-            childGrid.classList.add('square');
-            childGrid.addEventListener('mouseover', function(e){
-                this.style.backgroundColor = color.value
-            })
-            clear.addEventListener('click', function(e){
-                childGrid.style.backgroundColor = 'white'
-                // console.log(this.classList)
-            })
-        }
-    }
+// equal column and row 
+grid.style.gridTemplateRows = `repeat(${range.value}, 1fr)`
+grid.style.gridTemplateColumns = `repeat(${range.value}, 1fr)`
+
+// to choose color
+function colorTrail(clr){
+    clr.addEventListener('mouseover', function(e){
+        this.style.backgroundColor = color.value
+    })
 }
-createSquares(numberOfSquares)
+// to clear screen 
+function clnBtn(clearScreen){
+    clean.addEventListener('click', function(){
+        clearScreen.style.backgroundColor = 'white'        
+    })
+}
+
+
+
+
+
+
+let x = range.value * range.value
+for(i=1; i <= x; i++){
+    const div = document.createElement('div')
+    div.classList.add('square')
+    grid.insertAdjacentElement('beforeend', div)
+    colorTrail(div)
+    clnBtn(div)
+}
