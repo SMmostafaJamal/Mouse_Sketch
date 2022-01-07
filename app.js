dc481d7f1f3a950382c667f8afdf27714511fca5
sm.mostafajamal <sm.mostafajamal@gmail.com>
2022-01-08 01:08:00 +0600

    completely working without ereaser

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
 const grid = document.querySelector('#grid');
 const color = document.querySelector('#color');
-const clean = document.querySelector('#clean');
+const cleanBtn = document.querySelector('#clean');
 const range = document.querySelector('#rangeValue')
+const output = document.querySelector('#displayRange')
+// console.log(range.defaultValue)
 
-// equal column and row 
-grid.style.gridTemplateRows = `repeat(${range.value}, 1fr)`
-grid.style.gridTemplateColumns = `repeat(${range.value}, 1fr)`
+
+
+
+
+
+
+
+startingValue(range.defaultValue)
+range.addEventListener('change', function(){
+    grid.innerHTML = ''  // to clean previous inner grids
+    startingValue(range.value)
+
+})
+
+// creating grids, clearing color and making color trails
+function createGridFunc(val){
+    for(i=0; i < val; i++){
+        const div = document.createElement('div')
+        grid.insertAdjacentElement('afterbegin', div)
+        div.classList.add('square')
+        colorTrail(div)
+        clean(div,range)
+        clean(div,cleanBtn)
+        
+    }
+
+}
+
+// for defaultValue and selected values
+function startingValue(rangeVal){
+    output.innerHTML = rangeVal
+    // equal column and row 
+    grid.style.gridTemplateRows = `repeat(${rangeVal}, 1fr)`
+    grid.style.gridTemplateColumns = `repeat(${rangeVal}, 1fr)`
+    displayRange = rangeVal * rangeVal
+    createGridFunc(displayRange)
+}
 
 // to choose color
 function colorTrail(clr){
@@ -13,23 +49,13 @@ function colorTrail(clr){
         this.style.backgroundColor = color.value
     })
 }
+
 // to clear screen 
-function clnBtn(clearScreen){
-    clean.addEventListener('click', function(){
-        clearScreen.style.backgroundColor = 'white'        
+function clean(clnCell, eventElement){
+    eventElement.addEventListener('click', function(){
+        clnCell.style.backgroundColor = 'white'      
     })
 }
 
 
 
-
-
-
-let x = range.value * range.value
-for(i=1; i <= x; i++){
-    const div = document.createElement('div')
-    div.classList.add('square')
-    grid.insertAdjacentElement('beforeend', div)
-    colorTrail(div)
-    clnBtn(div)
-}
